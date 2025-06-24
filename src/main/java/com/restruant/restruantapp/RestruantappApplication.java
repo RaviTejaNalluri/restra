2223483e946702f01fb9bddb75e699a6640f25b1
ravi.nalluri1998@gmail.com <37662523+RaviTejaNalluri@users.noreply.github.com>
2025-06-24 18:31:49 -0400

    Adding the inital files

---
+++ src/main/java/com/restruant/restruantapp/RestruantappApplication.java
@@ -0,0 +1,13 @@
+package com.restruant.restruantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestruantappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestruantappApplication.class, args);
+	}
+
+}
